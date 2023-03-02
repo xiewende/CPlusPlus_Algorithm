62913bee21e7351dcd50fba5ff523dd240e14c62
xiewende <2276230432@qq.com>
2023-03-02 10:14:01 +0800

    env testing

---
+++ test.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 12;
+    cout << a << endl;
+    cout << "Hello, world!" << endl;
+
+    int b = 3;
+    cout << b + a << endl;
+
+    // system("pause");
+    
+    return 0;
+}
