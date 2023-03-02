ef340dc0b8f4cda596c676f35c8d4cc739c27061
xiewende <2276230432@qq.com>
2023-03-02 10:15:18 +0800

    algorithm of queue

---
+++ queue/hello.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a = 12;
+    cout << a + 12 << endl;
+    cout << "Hello, world!" << endl;
+
+    system("pause");
+    return 0;
+}
