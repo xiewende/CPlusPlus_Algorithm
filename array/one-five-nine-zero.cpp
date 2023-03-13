c3e31603f1ea22f7f0f89821dc0f8c07d359ae21
xiewende <2276230432@qq.com>
2023-03-13 21:42:03 +0800

    1590. Make Remainder Array Sum Divisible by P

--- array/one-five-nine-zero.cpp
+++ array/one-five-nine-zero.cpp
@@ -1,9 +1,22 @@
 // 1590. 使数组和能被 P 整除
+// 给你一个正整数数组 nums，请你移除 最短 子数组（可以为 空），使得剩余元素的 和 能被 p 整除。 不允许 将整个数组都移除。
 
 // 解题思路 ： 前缀和 + HashMap
 // HsahMap ==> key: Sum % P  value：下标 index  求最短子数组需要index
 
-// 取余的一个定理： 同余： 若（x-y） % p == 0，则 x 和 y 对 模 p 同余 
+// 取余的一个定理： 定理一同余： 若（x-y） % p == 0，则 x 和 y 对 模 p 同余 
+//                 定理二：    若 (z-y) % p ==x 则 y % p  == （z-x） % p
+
+// sum[i]: 代表到数组 i 位置的数组前缀和； sum[n]: 数组全部和=x
+// 定理一
+// 根据题意，要使移除子数组后剩余数组能够被 p 整除，【sum[n] - (某个子数组)】 % p == 0 
+//                                           ==》sum[n] % p  == (某个子数组)】 % p
+//                                           ==》sum[n] % p  == 【sum[i]-sum[j]】 % p
+//                                           ==》x  == 【sum[i]-sum[j]】 % p  ( i > j )
+//定理二 
+//                                              【sum[i]-sum[j]】 % p = x      
+//                                               sum[j] % x == 【sum[i] - x】 % p                                                                     
+
 
 #include<iostream>
 #include<vector>
