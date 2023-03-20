2aeb2cd57f134aeaae235ae8838d425be342a350
xiewende <2276230432@qq.com>
2023-03-20 22:05:49 +0800

    2389. and Finite Longest Subsequence

---
+++ array/two-three-eight-nine.cpp
@@ -0,0 +1,37 @@
+// 2389. 和有限的最长子序列
+
+#include<iostream>
+#include<vector>
+#include<algorithm>
+
+using namespace std;
+
+vector<int> answerQueries(vector<int>& nums, vector<int>& queries) {
+
+    int n = nums.size(), m = queries.size();
+    sort(nums.begin(), nums.end());
+    vector<int> f(n + 1);
+    for (int i = 0; i < n; i++) {
+        f[i + 1] = f[i] + nums[i];
+    }
+    vector<int> answer(m);
+    for (int i = 0; i < m; i++) {
+        answer[i] = upper_bound(f.begin(), f.end(), queries[i]) - f.begin() - 1;
+    }
+
+    return answer;
+}
+
+int main(){
+
+    vector<int> nums{4,5,2,1};
+    vector<int> queries{3, 10, 21};
+
+    vector<int> ans = answerQueries(nums, queries);
+
+    for(int zhi : ans){
+        cout << zhi << " ";
+    }
+
+    return 0;
+}
