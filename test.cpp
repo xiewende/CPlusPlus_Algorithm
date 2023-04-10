36bff2d34c3f5457c883b0e3dfe0eb916ce0c320
xiewende <2276230432@qq.com>
2023-04-10 22:04:57 +0800

    test

--- test.cpp
+++ test.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
+#include<vector>
 #include<string>
+#include<algorithm>
 
 using namespace std;
+
+// 自定义排序方式，也可以排序结构体，多个维度
+// 在定义为 类成员函数 的时候一定需要 static 修饰；
+bool cmp(int a, int b){
+    // return a>b; // 降序
+    return a < b; // 升序
+}
 
 int main()
 {
@@ -11,6 +20,13 @@ int main()
 
     int b = 3;
     cout << b + a << endl;
+
+    vector<int> nums{3, 4, 2, 5};
+    sort(nums.begin(), nums.end(), cmp);
+    for (int i = 0; i < nums.size();i++)
+    {
+        cout << nums[i] << " ";
+    }
 
     //system("pause");
     return 0;
