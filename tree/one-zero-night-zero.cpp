fc70568f3e79ad8e2a375445ef3d9052f18b8b54
xiewende <2276230432@qq.com>
2023-06-01 22:20:38 +0800

    1080. Insufficient nodes on the root-to-leaf path

---
+++ tree/one-zero-night-zero.cpp
@@ -0,0 +1,39 @@
+// 1080. 根到叶路径上的不足节点
+
+#include<iostream>
+#include<vector>
+
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+
+bool checkSufficientLeaf(TreeNode *node, int sum, int limit) {
+    if (!node) {
+        return false;
+    }
+    if (node->left == nullptr && node->right == nullptr) {
+        return node->val + sum >= limit;
+    }
+    bool haveSufficientLeft = checkSufficientLeaf(node->left, sum + node->val, limit);
+    bool haveSufficientRight = checkSufficientLeaf(node->right, sum + node->val, limit);
+    if (!haveSufficientLeft) {
+        node->left = nullptr;
+    }
+    if (!haveSufficientRight) {
+        node->right = nullptr;
+    }
+    return haveSufficientLeft || haveSufficientRight;
+}
+
+TreeNode* sufficientSubset(TreeNode* root, int limit) {
+    bool haveSufficient = checkSufficientLeaf(root, 0, limit);
+    return haveSufficient ? root : nullptr;
+}
